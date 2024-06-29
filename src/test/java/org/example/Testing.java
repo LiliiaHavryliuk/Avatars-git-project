84b3dbb262342087638c82f8faa45524b646843a
LiliiaHavryliuk <liliysgav5@gmail.com>
2024-06-28 23:36:47 -0400

    My four push

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -4,7 +4,9 @@ public class Testing {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("I Am having the time of my life");
-        System.out.println("I CANNOT WAIT TO GET A JOB");
+        System.out.println("I want to go to the ocean");
+        System.out.println("I love my family");
+        System.out.println("Bye, bye");
 
     }
 
