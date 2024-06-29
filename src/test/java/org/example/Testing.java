d1f4c6bac43fb51ca588c1ed7c73b3545bf989e9
LiliiaHavryliuk <liliysgav5@gmail.com>
2024-06-28 23:04:26 -0400

    My third push

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Testing {
     public static void main(String[] args) {
-        System.out.println("My third git class");
+        System.out.println("Hello world");
+        System.out.println("I Am having the time of my life");
+        System.out.println("I cannot wait to get a job");
     }
 
 }
