3734932678de6e15535b05f70f30db9058514120
LiliiaHavryliuk <liliysgav5@gmail.com>
2024-06-28 23:20:30 -0400

    My third push

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -4,7 +4,8 @@ public class Testing {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("I Am having the time of my life");
-        System.out.println("I cannot wait to get a job");
+        System.out.println("I CANNOT WAIT TO GET A JOB");
+
     }
 
 }
